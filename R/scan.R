9e26bb1cc15055bca97ccbcb859b7e2e6a1ab538
andrew <arenn@design.upenn.edu>
2020-03-16 10:09:34 -0400

    Commit from RStudio

--- R/scan.R
+++ R/scan.R
@@ -7,14 +7,39 @@ library(lubridate)
 read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv") %>%
   clean_names() %>%
   filter(country_region == "US") %>%
-  gather(date, cases, x1_22_20:x3_10_20) %>%
+  gather(date, cases, x1_22_20:x3_15_20) %>%
+  mutate(date = str_remove_all(date, "x")) %>%
+  mutate(date = str_replace_all(date, "_", "-")) %>%
+  mutate(date = mdy(date)) %>%
+  filter(date > as_date('2020-02-20')) %>%
+  group_by(date) %>%
+  summarise(cases = sum(cases)) %>%
+  ggplot(aes(date, cases)) +
+  geom_point() +
+  geom_smooth()
+
+read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv") %>%
+  clean_names() %>%
+  filter(country_region == "US") %>%
+  gather(date, cases, x1_22_20:x3_15_20) %>%
+  mutate(date = str_remove_all(date, "x")) %>%
+  mutate(date = str_replace_all(date, "_", "-")) %>%
+  mutate(date = mdy(date)) %>%
+  group_by(date) %>%
+  summarise(cases = sum(cases)) %>%
+  arrange(desc(date))
+
+read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv") %>%
+  clean_names() %>%
+  filter(country_region == "US") %>%
+  gather(date, cases, x1_22_20:x3_15_20) %>%
   mutate(date = str_remove_all(date, "x")) %>%
   mutate(date = str_replace_all(date, "_", "-")) %>%
   mutate(date = mdy(date)) %>%
   group_by(date) %>%
   summarise(cases = sum(cases)) %>%
   ggplot(aes(date, cases)) +
-  geom_point()
+  geom_bar(stat = 'identity')
 
 ##
 
@@ -53,4 +78,18 @@ read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_
   leaflet() %>% 
   addTiles() %>%
   addMarkers(~long, ~lat, popup = ~as.character(cases), label = ~as.character(cases))
+
+##
+
+read_csv("https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv") %>%
+  clean_names() %>%
+  filter(country_region == "US") %>%
+  gather(date, cases, x1_22_20:x3_15_20) %>%
+  mutate(date = str_remove_all(date, "x")) %>%
+  mutate(date = str_replace_all(date, "_", "-")) %>%
+  mutate(date = mdy(date)) %>%
+  group_by(date) %>%
+  summarise(cases = sum(cases)) %>%
+  arrange(desc(date))
+
 
