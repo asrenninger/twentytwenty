aff22ba6226b82e7ad3e05d6913d424852c570ab
andrew <arenn@design.upenn.edu>
2020-03-18 21:36:10 -0400

    Commit from RStudio

--- R/scan.R
+++ R/scan.R
@@ -7,7 +7,7 @@ library(lubridate)
 read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv") %>%
   clean_names() %>%
   filter(country_region == "US") %>%
-  gather(date, cases, x1_22_20:x3_15_20) %>%
+  gather(date, cases, x1_22_20:x3_16_20) %>%
   mutate(date = str_remove_all(date, "x")) %>%
   mutate(date = str_replace_all(date, "_", "-")) %>%
   mutate(date = mdy(date)) %>%
@@ -21,7 +21,7 @@ read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_
 read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv") %>%
   clean_names() %>%
   filter(country_region == "US") %>%
-  gather(date, cases, x1_22_20:x3_15_20) %>%
+  gather(date, cases, x1_22_20:x3_16_20) %>%
   mutate(date = str_remove_all(date, "x")) %>%
   mutate(date = str_replace_all(date, "_", "-")) %>%
   mutate(date = mdy(date)) %>%
@@ -33,13 +33,42 @@ read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_
   mutate(coefficient = change / change_lag) %>%
   arrange(desc(date))
 
+library(tigris)
+
+states <- states(class = 'sf') %>%
+  clean_names()
+
+library(geofacet)
+
 read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv") %>%
   clean_names() %>%
   filter(country_region == "US") %>%
-  gather(date, cases, x1_22_20:x3_15_20) %>%
+  gather(date, cases, x1_22_20:x3_16_20) %>%
   mutate(date = str_remove_all(date, "x")) %>%
   mutate(date = str_replace_all(date, "_", "-")) %>%
   mutate(date = mdy(date)) %>%
+  st_as_sf(coords = c("long", "lat"), crs = 4269) %>%
+  st_join(states) %>%
+  st_drop_geometry() %>%
+  group_by(date, name) %>%
+  summarise(cases = sum(cases)) %>%
+  mutate(cases_lag = lag(cases)) %>%
+  mutate(change = cases - cases_lag) %>%
+  mutate(change_lag = lag(change)) %>%
+  mutate(coefficient = change / change_lag) %>%
+  filter(cases > 0) %>%
+  ggplot(aes(date, coefficient)) +
+  geom_bar(stat = 'identity') +
+  facet_wrap(~ name, scales = 'free')
+
+read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv") %>%
+  clean_names() %>%
+  filter(country_region == "US") %>%
+  gather(date, cases, x1_22_20:x3_16_20) %>%
+  mutate(date = str_remove_all(date, "x")) %>%
+  mutate(date = str_replace_all(date, "_", "-")) %>%
+  mutate(date = mdy(date)) %>%
+  filter(date > as_date("2020-02-20")) %>%
   group_by(date) %>%
   summarise(cases = sum(cases)) %>%
   ggplot(aes(date, cases)) +
@@ -59,6 +88,7 @@ read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_
   mutate(date = str_replace_all(date, "_", "-")) %>%
   mutate(date = mdy(date)) %>%
   st_as_sf(coords = c("long", "lat")) %>%
+  filter(!province_state %in% states$name) %>%
   group_by(province_state) %>%
   summarise(cases = sum(cases)) %>%
   mapview::mapview(zcol = "cases")
@@ -72,16 +102,17 @@ library(leaflet)
 read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv") %>%
   clean_names() %>%
   filter(country_region == "US") %>%
-  gather(date, cases, x1_22_20:x3_10_20) %>%
+  gather(date, cases, x1_22_20:x3_17_20) %>%
   mutate(date = str_remove_all(date, "x")) %>%
   mutate(date = str_replace_all(date, "_", "-")) %>%
   mutate(date = mdy(date)) %>%
   st_as_sf(coords = c("long", "lat"), remove = FALSE) %>%
+  filter(!province_state %in% states$name) %>%
   group_by(province_state, long, lat) %>%
   summarise(cases = sum(cases)) %>% 
   leaflet() %>% 
   addTiles() %>%
-  addMarkers(~long, ~lat, popup = ~as.character(cases), label = ~as.character(cases))
+  addCircles(~long, ~lat, popup = ~as.character(cases), radius = ~cases *100, fill = ~cases, label = ~as.character(cases))
 
 ##
 
