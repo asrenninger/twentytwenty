bdf826c9b43fcc08ac8737b2c9fa070821ed1571
andrew <arenn@design.upenn.edu>
2020-03-22 20:42:12 -0400

    Commit from Rstudio

--- R/scan.R
+++ R/scan.R
@@ -7,7 +7,7 @@ library(lubridate)
 read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv") %>%
   clean_names() %>%
   filter(country_region == "US") %>%
-  gather(date, cases, x1_22_20:x3_16_20) %>%
+  gather(date, cases, x1_22_20:x3_20_20) %>%
   mutate(date = str_remove_all(date, "x")) %>%
   mutate(date = str_replace_all(date, "_", "-")) %>%
   mutate(date = mdy(date)) %>%
@@ -21,7 +21,7 @@ read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_
 read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv") %>%
   clean_names() %>%
   filter(country_region == "US") %>%
-  gather(date, cases, x1_22_20:x3_18_20) %>%
+  gather(date, cases, x1_22_20:x3_20_20) %>%
   mutate(date = str_remove_all(date, "x")) %>%
   mutate(date = str_replace_all(date, "_", "-")) %>%
   mutate(date = mdy(date)) %>%
@@ -39,27 +39,6 @@ states <- states(class = 'sf') %>%
   clean_names()
 
 library(geofacet)
-
-read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv") %>%
-  clean_names() %>%
-  filter(country_region == "US") %>%
-  gather(date, cases, x1_22_20:x3_16_20) %>%
-  mutate(date = str_remove_all(date, "x")) %>%
-  mutate(date = str_replace_all(date, "_", "-")) %>%
-  mutate(date = mdy(date)) %>%
-  st_as_sf(coords = c("long", "lat"), crs = 4269) %>%
-  st_join(states) %>%
-  st_drop_geometry() %>%
-  group_by(date, name) %>%
-  summarise(cases = sum(cases)) %>%
-  mutate(cases_lag = lag(cases)) %>%
-  mutate(change = cases - cases_lag) %>%
-  mutate(change_lag = lag(change)) %>%
-  mutate(coefficient = change / change_lag) %>%
-  filter(cases > 0) %>%
-  ggplot(aes(date, coefficient)) +
-  geom_bar(stat = 'identity') +
-  facet_wrap(~ name, scales = 'free')
 
 read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv") %>%
   clean_names() %>%
