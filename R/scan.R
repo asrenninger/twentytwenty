61f4bc469620cc6ff4db27b34d8c3a520d079ebc
andrew <arenn@design.upenn.edu>
2020-03-31 21:07:57 -0400

    Commit from RStudio

--- R/scan.R
+++ R/scan.R
@@ -78,7 +78,7 @@ library(leaflet)
 
 ##
 
-pal <- colorNumeric("viridis", NULL)
+pal <- colorQuantile("viridis", NULL, n = 3)
 
 read_csv("https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_daily_reports/03-23-2020.csv") %>%
   clean_names() %>%
@@ -89,7 +89,7 @@ read_csv("https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_da
   filter(cases > 0) %>%
   leaflet() %>% 
   addTiles() %>%
-  addCircles(~long, ~lat, popup = ~as.character(cases), radius = ~sqrt(cases), color = ~pal(cases), label = ~as.character(cases))
+  addCircles(~long, ~lat, popup = ~as.character(cases), radius = ~sqrt(cases * 100), color = ~pal(cases), label = ~as.character(cases))
 
 ##
 
@@ -118,5 +118,100 @@ covid <-
   filter(!province_state %in% states$name) %>%
   group_by(province_state, long, lat) %>%
   summarise(cases = sum(cases))
+
+##
+
+read_csv("https://github.com/nytimes/covid-19-data/raw/master/us-counties.csv") %>%
+  filter(date > as_date('2020-02-29')) %>%
+  group_by(date) %>%
+  summarise(cases = sum(cases)) %>%
+  mutate(cases_lag = lag(cases)) %>%
+  mutate(change = cases - cases_lag) %>%
+  mutate(change_lag = lag(change)) %>%
+  mutate(coefficient = change / change_lag) %>%
+  arrange(desc(date)) %>%
+  ggplot(aes(date, coefficient)) +
+  geom_bar(stat = 'identity')
+
+metro <- c("Bucks", "Chester", "Delaware", "Montgomery", "Philadelphia", "Burlington", "Camden", "Gloucester", "New Castle")
+  
+p1 <- read_csv("https://github.com/nytimes/covid-19-data/raw/master/us-counties.csv") %>%
+  filter(date > as_date('2020-02-29')) %>%
+  filter(state == "New Jersey" | state == "Pennsylvania" | state == "Delaware") %>%
+  filter(county %in% metro) %>%
+  group_by(date) %>%
+  summarise(cases = sum(cases)) %>%
+  mutate(cases_lag = lag(cases)) %>%
+  mutate(change = cases - cases_lag) %>%
+  mutate(change_lag = lag(change)) %>%
+  mutate(multiplier = change / change_lag) %>%
+  arrange(desc(date)) %>%
+  ggplot(aes(date, multiplier)) +
+  geom_bar(stat = 'identity', show.legend = FALSE) +
+  theme_bw()
+
+p2 <- read_csv("https://github.com/nytimes/covid-19-data/raw/master/us-counties.csv") %>%
+  filter(date > as_date('2020-02-29')) %>%
+  filter(state == "New Jersey" | state == "Pennsylvania" | state == "Delaware") %>%
+  filter(county %in% metro) %>%
+  group_by(date) %>%
+  summarise(cases = sum(cases)) %>%
+  mutate(cases_lag = lag(cases)) %>%
+  mutate(change = cases - cases_lag) %>%
+  mutate(change_lag = lag(change)) %>%
+  mutate(coefficient = change / change_lag) %>%
+  arrange(desc(date)) %>%
+  ggplot(aes(date, change)) +
+  geom_bar(stat = 'identity', show.legend = FALSE) +
+  theme_bw()
+
+p3 <- read_csv("https://github.com/nytimes/covid-19-data/raw/master/us-counties.csv") %>%
+  filter(date > as_date('2020-02-29')) %>%
+  filter(state == "New Jersey" | state == "Pennsylvania" | state == "Delaware") %>%
+  filter(county %in% metro) %>%
+  group_by(date, county) %>%
+  summarise(cases = sum(cases)) %>%
+  mutate(cases_lag = lag(cases)) %>%
+  mutate(change = cases - cases_lag) %>%
+  mutate(change_lag = lag(change)) %>%
+  mutate(coefficient = change / change_lag) %>%
+  arrange(desc(date)) %>%
+  ggplot(aes(date, cases, fill = county)) +
+  geom_bar(stat = 'identity', position = 'stack') +
+  theme_bw()
+
+library(patchwork)
+
+p <- p1 + p2 + p3
+
+ggsave("region.png", height = 6, width = 18, dpi = 300)
+
+##
+
+p1 <- read_csv("~/Downloads/multiTimeline.csv", skip = 1) %>%
+  gather(Term, Searches, 2:4) %>%
+  mutate(Term = str_remove_all(Term, ": \\(United States\\)")) %>%
+  ggplot(aes(Day, Searches, color = Term)) +
+  geom_line(size = 2) +
+  ylab("Searches (% total)") +
+  scale_color_brewer(palette = 'Set1') +
+  theme_hor() +
+  ggsave("searches.png", height = 5, width = 8, dpi = 300)
+
+p2 <- read_csv("~/Downloads/covid-19-wuhan-virus-chinese-v-stories-over-time-20200331151417.csv") %>%
+  select(date, contains("ratio")) %>%
+  gather(Term, Searches, 2:4) %>%
+  mutate(Term = str_remove_all(Term, "-ratio")) %>%
+  ggplot(aes(date, Searches, color = Term)) +
+  geom_line(size = 2) +
+  ylab("Articles (% total)") +
+  xlab("Day") +
+  scale_color_brewer(palette = 'Set1') +
+  theme_hor() +
+  ggsave("articles.png", height = 5, width = 8, dpi = 300)
+
+p <- p1 + p2 + plot_layout(guides = 'collect') + plot_annotation(caption = "Source: Google, Media Cloud")
+
+ggsave("coverage.png", height = 5, width = 16, dpi = 300)
 
 
